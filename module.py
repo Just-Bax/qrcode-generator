7c7bb20688e380d453dea4149215b6a089a9a466
Isfandiyor Baxtiyorov <isfandiyorbaxtiyorov132@gmail.com>
2024-05-29 22:38:08 +0500

    Make trackor and field as params

--- module.py
+++ module.py
@@ -12,20 +12,20 @@ class Generator:
         self.back_color = back_color
         self.fill_color = fill_color
 
-    def generate_qrcode(self, asset_id, asset_name):
+    def generate_qrcode(self, value, title):
         qr = qrcode.QRCode(version=1, box_size=12, border=6)
-        qr.add_data(asset_id); qr.make(fit=True)
+        qr.add_data(value); qr.make(fit=True)
         img = qr.make_image(back_color=self.back_color, fill_color=self.fill_color)
         qr_width, qr_height = img.size
         space_height = qr_height/6
         font = ImageFont.truetype("arial.ttf", 16)
         draw = ImageDraw.Draw(img)
         # Asset Name
-        _, _, txt_width, txt_height = draw.textbbox((0, 0), text=asset_name, font=font)
-        draw.text(((qr_width-txt_width)/2, (space_height-txt_height)/2), text=asset_name, fill=(255, 0, 0), font=font)
+        _, _, txt_width, txt_height = draw.textbbox((0, 0), text=title, font=font)
+        draw.text(((qr_width-txt_width)/2, (space_height-txt_height)/2), text=title, fill=(255, 0, 0), font=font)
         # Asset ID
-        _, _, txt_width, txt_height = draw.textbbox((0, 0), text=asset_id, font=font)
-        draw.text(((qr_width-txt_width)/2, ((qr_height-space_height)+(txt_height/2))), text=asset_id, fill=(0, 0, 0), font=font)
+        _, _, txt_width, txt_height = draw.textbbox((0, 0), text=value, font=font)
+        draw.text(((qr_width-txt_width)/2, ((qr_height-space_height)+(txt_height/2))), text=value, fill=(0, 0, 0), font=font)
         # Company Name
         # _, _, txt_width, txt_height = draw.textbbox((0, 0), text=company_name, font=font)
         # draw.text(((qr_width-txt_width)/2, ((qr_height-space_height)+(txt_height*2))), text=company_name, fill=(255, 0, 0), font=font)
@@ -68,19 +68,14 @@ class OVTrackor:
             raise ModuleError('Failed to get_trackors_by_fields_and_search_trigger', self.trackor_type_wrapper.errors)
         return list(self.trackor_type_wrapper.jsonData)
 
-    def update_fields_by_trackor_id(self, trackor_key: str, trackor_id: int, field_dict: Dict[str, Dict[str, Any]]) -> Dict[str, Any]:
+    def update_fields_by_trackor_id(self, trackor_id: int, field_dict: Dict[str, Dict[str, Any]]) -> Dict[str, Any]:
         self.trackor_type_wrapper.update(trackorId=trackor_id, fields=field_dict)
         if len(self.trackor_type_wrapper.errors) > 0:
-            raise ModuleError(f'Failed to update_fields_by_trackor_id for {trackor_key}', self.trackor_type_wrapper.errors)
+            raise ModuleError(f'Failed to update_fields_by_trackor_id', self.trackor_type_wrapper.errors)
         return self.trackor_type_wrapper.jsonData
 
 class Module:
     TRACKOR_ID = 'TRACKOR_ID'
-    TRACKOR_KEY = 'TRACKOR_KEY'
-    ASSET_ITEM_TRACKOR_TYPE = 'EC_ASSET_ITEMS'
-    ASSET_NAME_FIELDNAME = 'ECAI_ASSET_NAME'
-    GENERATE_FIELDNAME = 'ECAI_GENERATE_QR_CODE'
-    QRCODE_FIELDNAME = 'ECAI_ASSET_QR_CODE'
 
     def __init__(self, ov_module_log: IntegrationLog, settings_data: dict):
         self._module_log = ov_module_log
@@ -88,26 +83,32 @@ class Module:
         self._ov_access_parameters = OVAccessParameters(settings_data['ovUrl'], settings_data['ovAccessKey'], settings_data['ovSecretKey'])
         self._ov_trackor = OVTrackor(self._ov_access_parameters)
         self._generator = Generator(fill_color=settings_data["qrcodeFillColor"], back_color=settings_data["qrcodeBackgroudColor"])
+        # Qrcode settings
+        self._trackor_type = settings_data["trackorType"]
+        self._qrcode_generate_field = settings_data["qrcodeGenerateField"]
+        self._qrcode_image_field = settings_data["qrcodeImageField"]
+        self._qrcode_title_field = settings_data["qrcodeTitleField"]
+        self._qrcode_value_field = settings_data["qrcodeValueField"]
 
     def start(self):
         # self._module_log.add(LogLevel.INFO, 'Module is started')
-        self._ov_trackor.trackor_type_wrapper = self.ASSET_ITEM_TRACKOR_TYPE
-        asset_items_fields_list = [self.TRACKOR_KEY, self.ASSET_NAME_FIELDNAME]
-        asset_items_search_trigger = f"equal({self.GENERATE_FIELDNAME}, 1)"
-        asset_items = self._ov_trackor.get_trackors_by_fields_and_search_trigger(fields_list=asset_items_fields_list, search_trigger=asset_items_search_trigger)
+        self._ov_trackor.trackor_type_wrapper = self._trackor_type
+        qrcode_fields_list = [self._qrcode_title_field, self._qrcode_value_field]
+        qrcode_search_trigger = f"equal({self._qrcode_generate_field}, 1)"
+        asset_items = self._ov_trackor.get_trackors_by_fields_and_search_trigger(fields_list=qrcode_fields_list, search_trigger=qrcode_search_trigger)
         for asset_item in asset_items:
             trackor_id = asset_item[self.TRACKOR_ID]
-            trackor_key = asset_item[self.TRACKOR_KEY]
-            asset_name = asset_item[self.ASSET_NAME_FIELDNAME]
-            qrcode_bytes = self._generator.generate_qrcode(asset_id=trackor_key, asset_name=asset_name)
+            qrcode_value = asset_item[self._qrcode_value_field]
+            qrcode_title = asset_item[self._qrcode_title_field]
+            qrcode_bytes = self._generator.generate_qrcode(value=qrcode_value, title=qrcode_title)
             asset_item_fields = {
-                self.GENERATE_FIELDNAME: '0',
-                self.QRCODE_FIELDNAME: {
-                    "file_name": f"{trackor_key}.png",
+                self._qrcode_generate_field: '0',
+                self._qrcode_image_field: {
+                    "file_name": f"{qrcode_value}.png",
                     "data": qrcode_bytes
                 }
             }
-            self._ov_trackor.update_fields_by_trackor_id(trackor_key=trackor_key, trackor_id=trackor_id, field_dict=asset_item_fields)
+            self._ov_trackor.update_fields_by_trackor_id(trackor_id=trackor_id, field_dict=asset_item_fields)
             # self._module_log.add(LogLevel.INFO, f'Qrcode generated: trackor_id={trackor_id}, trackor_key={trackor_key}')
 
 if __name__ == "__main__":
